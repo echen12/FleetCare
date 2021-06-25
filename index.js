441372dceb017d4cc294e88b485d0e27e4e37ff7
Edmond chen <chen.edmond13@gmail.com>
2021-06-24 18:14:22 -0700

    push to heroku for hosting

--- index.js
+++ index.js
@@ -2,139 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(express.json())
-app.use(cors())
 const Vehicle = require('./models/vehicleInfo')
 
-// let vehicles = [
-//     {
-//         "make": "TOYOTA",
-//         "model": "Tacoma",
-//         "modelYear": "2016",
-//         "vehicleType": "Pickup",
-//         "vin": "5TFDZ5BN3GX010438",
-//         "plateNumber": "AB1234",
-//         "insuranceExpiryDate": "2021-07-22",
-//         "color": "white",
-//         "mileageInformation": [
-//             {
-//                 "lastUpdated": "06/20/2021",
-//                 "lastMileage": 0,
-//                 "oilChangeStartInterval": false
-//             },
-//             {
-//                 "lastUpdated": "06/20/2021",
-//                 "lastMileage": "3000",
-//                 "oilChangeStartInterval": false
-//             },
-//             {
-//                 "lastUpdated": "06/20/2021",
-//                 "lastMileage": "4500",
-//                 "oilChangeStartInterval": false
-//             },
-//             {
-//                 "lastUpdated": "06/20/2021",
-//                 "lastMileage": "5000",
-//                 "oilChangeStartInterval": true,
-//                 "oilChangeInterval": "16000"
-//             },
-//             {
-//                 "lastUpdated": "06/20/2021",
-//                 "lastMileage": "7000",
-//                 "oilChangeStartInterval": false
-//             }
-//         ],
-//         "warrantyInfo": [
-//             {
-//                 "warrantyTitle": "fdfd",
-//                 "warrantyProvider": "dfaf",
-//                 "warrantyDetail": "fdfd",
-//                 "warrantyDate": "06/20/2021"
-//             },
-//             {
-//                 "warrantyTitle": "AC Compressor Clutch",
-//                 "warrantyProvider": "Toyota",
-//                 "warrantyDetail": "Replaced AC compressor under warranty, recharged system",
-//                 "warrantyDate": "06/20/2021"
-//             }
-//         ],
-//         "id": 1
-//     },
-//     {
-//         "make": "GMC",
-//         "model": "Sierra",
-//         "modelYear": "2021",
-//         "vehicleType": "Pickup",
-//         "vin": "1GTU9CED4MZ361964",
-//         "plateNumber": "ABC1234",
-//         "insuranceExpiryDate": "2022-07-12",
-//         "color": "white",
-//         "mileageInformation": [
-//             {
-//                 "lastUpdated": "06/20/2021",
-//                 "lastMileage": 0,
-//                 "oilChangeStartInterval": false
-//             },
-//             {
-//                 "lastUpdated": "06/20/2021",
-//                 "lastMileage": "2353",
-//                 "oilChangeStartInterval": false
-//             }
-//         ],
-//         "warrantyInfo": [
-//             {
-//                 "warrantyTitle": "ABS Module",
-//                 "warrantyProvider": "GM",
-//                 "warrantyDetail": "replaced abs and abc module under warranty @ 4500km",
-//                 "warrantyDate": "06/20/2021"
-//             }
-//         ],
-//         "id": 2
-//     }
-// ]
-
-// const url = `mongodb+srv://fleetcare-user:fleetcare1@cluster0.ltn6m.mongodb.net/vehicleInfo?retryWrites=true&w=majority`
-
-// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-// const vehicleSchema = new mongoose.Schema({
-//     make: String,
-//     model: String,
-//     modelYear: String,
-//     vehicleType: String,
-//     vin: String,
-//     plateNumber: String,
-//     insuranceExpiryDate: String,
-//     color: String,
-//     mileageInformation: [
-//         {
-//             lastUpdated: String,
-//             lastMileage: String,
-//             oilChangeStartInterval: Boolean,
-//             oilChangeInterval: String
-//         }
-//     ],
-//     warrantyInfo: [
-//         {
-//             warrantyTitle: String,
-//             warrantyProvider: String,
-//             warrantyDetail: String,
-//             warrantyDate: String
-//         }
-//     ]
-// })
-
-// const Vehicle = mongoose.model('Vehicle', vehicleSchema)
-
-// vehicleSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//         returnedObject.id = returnedObject._id.toString()
-//         delete returnedObject._id
-//         delete returnedObject.__v
-//     }
-// })
-
-//const Vehicle = mongoose.model('Vehicle', vehicleSchema)
+app.use(express.json())
+app.use(cors())
 
 
 // get api
@@ -145,7 +16,7 @@ app.get('/api/vehicle-info', (request, response) => {
 })
 
 // get all vehicles
-app.get('/vehicle-info', (request, response) => {
+app.get('/vehicle-info', async (request, response) => {
     Vehicle.find({}).then(vehicle => {
         response.json(vehicle)
     })
@@ -167,7 +38,8 @@ app.delete('/vehicle-info/:id', (request, response) => {
 })
 
 // add vehicle
-app.post('/vehicle-info', (request, response) => {
+app.post('/vehicle-info', async (request, response) => {
+
     if (!request.body.vin) {
         return response.status(400).json({
             error: 'VIN is missing!'
@@ -175,9 +47,11 @@ app.post('/vehicle-info', (request, response) => {
     }
 
     const vehicle = new Vehicle(request.body)
+
     vehicle.save().then(result => {
         response.json(result)
     })
+
 })
 
 // update vehicle details
@@ -188,6 +62,6 @@ app.put('/vehicle-info/:id', (request, response) => {
 
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
