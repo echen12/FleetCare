619fa8d82c5f5476ac2fc1e6f206a0f8be3136f0
Edmond chen <chen.edmond13@gmail.com>
2021-06-24 20:09:29 -0700

    updated code to work with heroku

--- index.js
+++ index.js
@@ -65,6 +65,17 @@ app.put('/vehicle-info/:id', (request, response) => {
 
 })
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
